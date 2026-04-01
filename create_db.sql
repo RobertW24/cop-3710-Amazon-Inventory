9c969dda4d279e12a853abb9ecde1e43881a98e0
Robert Waterman <rwaterman0451@floridapoly.edu>
2026-04-01 18:54:19 -0400

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -8,23 +8,8 @@ END;
 CREATE TABLE supplier (
 sup_id NUMBER PRIMARY KEY NOT NULL,
 sup_name VARCHAR2(20) NOT NULL,
-supply_price NUMBER(12,2) NOT NULL, 
+supply_price FLOAT NOT NULL, 
 lead_time VARCHAR2(10) NOT NULL
-);
-
-CREATE TABLE price (
-price_id NUMBER PRIMARY KEY NOT NULL,
-normal_price FLOAT NOT NULL,
-discount_percentage FLOAT NOT NULL,
-discounted_price FLOAT NOT NULL
-);
-
-CREATE TABLE product_info (
-prod_info_id NUMBER PRIMARY KEY NOT NULL,
-price_id NUMBER NOT NULL,
-prod_name VARCHAR2(500) NOT NULL,
-category VARCHAR2(200) NOT NULL,
-descript VARCHAR2(3000) NOT NULL
 );
 
 CREATE TABLE product_ (
@@ -37,21 +22,35 @@ rev_count NUMBER NOT NULL,
 avg_rating FLOAT NOT NULL
 );
 
-CREATE TABLE customer (
-cus_id VARCHAR2(300) PRIMARY KEY NOT NULL,
-cus_name VARCHAR2(200) NOT NULL
+CREATE TABLE product_info (
+prod_info_id NUMBER PRIMARY KEY NOT NULL,
+price_id NUMBER NOT NULL,
+prod_name VARCHAR2(500) NOT NULL,
+category VARCHAR2(200) NOT NULL,
+descript VARCHAR2(3000) NOT NULL
+);
+
+CREATE TABLE price (
+price_id NUMBER PRIMARY KEY NOT NULL,
+normal_price FLOAT NOT NULL,
+discount_percentage FLOAT NOT NULL,
+discounted_price FLOAT NOT NULL
 );
 
 CREATE TABLE review (
 prod_id VARCHAR2(20) NOT NULL,
-cus_id VARCHAR2(1000) NOT NULL,
-rev_title VARCHAR2(1000) NOT NULL,
+cus_id VARCHAR2(300) NOT NULL,
+rev_title VARCHAR2(500) NOT NULL,
 rev_body VARCHAR2(4000) NOT NULL,
 rating NUMBER NOT NULL,
 
 PRIMARY KEY (prod_id, cus_id)
 );
 
+CREATE TABLE customer (
+cus_id VARCHAR2(300) PRIMARY KEY NOT NULL,
+cus_name VARCHAR2(200) NOT NULL
+);
 
 
 
@@ -71,7 +70,7 @@ FOREIGN KEY (price_id)
 REFERENCES price (price_id);
 
 ALTER TABLE review
-ADD CONSTRAINT fk_review_product_
+ADD CONSTRAINT fk_review_product
 FOREIGN KEY (prod_id)
 REFERENCES product_ (prod_id);
 
